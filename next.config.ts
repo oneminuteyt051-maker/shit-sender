0c9376bb00fef405a1fe65ef3fd13bc50d0e5b3d
HarryObrain <oneminuteyt051@gmail.com>
2026-01-09 11:54:12 +0100

    Update CORS headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig = {
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, Accept-Encoding" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, Content-Encoding, x-accept-sdk-version, x-accept-blockchain-ids, x-action-version, x-blockchain-ids" },
+                    { key: "Access-Control-Expose-Headers", value: "x-action-version, x-blockchain-ids" },
                 ],
             },
         ];
