858f147e47daa53f2a0651d25610686ee089ef3a
HarryObrain <oneminuteyt051@gmail.com>
2026-01-09 20:56:28 +0100

    Update and rename tailwind.config.ts to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        comic: ['"Comic Sans MS"', "Chalkboard SE", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
